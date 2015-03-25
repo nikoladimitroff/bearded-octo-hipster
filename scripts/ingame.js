9f013e20aaff037996f0deb4825c0ca9389033c3
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2015-03-25 02:38:55 +0200

    [Enhancement] Views now use knockout which greatly eases changing the content.

--- scripts/ingame.js
+++ scripts/ingame.js
@@ -1,2 +1,15 @@
+var data = {
+    ingameMenu: [
+        "Resume",
+        "Map",
+        "Inventory",
+        "Settings",
+        "Quit"
+    ]
+};
+
+ko.applyBindings(data);
+
+
 BindCircularMenu(document.querySelector(".gamepad#ingame-menu .cn-button"),
                  document.querySelector(".gamepad#ingame-menu .cn-wrapper"));
--- scripts/main_menu.js
+++ scripts/main_menu.js
@@ -1,2 +1,58 @@
+var data = {
+    heading: "Our Awesome Game",
+    menu: [{
+        name: "New Game",
+        icon: "",
+        children: [
+            {name: "Single Player", icon: ""},
+            {name: "Versus", icon: ""},
+            {name: "Co-op", icon: ""},
+            {name: "Online", icon: ""}
+        ]}, {
+        name: "Settings",
+        icon: "",
+        children: [{
+            name: "Video",
+            icon: "",
+            children: [{
+                name: "Basic",
+                icon: "",
+                children: [
+                    {name: "Resolution", icon: ""},
+                    {name: "Anisotropic", icon: ""},
+                    {name: "Antialiasing", icon: ""},
+                ]}, {
+                name: "Advanced",
+                icon: "",
+                children: [
+                    {name: "Texture quality", icon: ""},
+                    {name: "Shadow quality", icon: ""},
+                    {name: "View distance", icon: ""},
+                ]}
+            ]}, {
+            name: "Audio",
+            icon: "",
+            children: [
+                {name: "Master volume", icon: ""},
+                {name: "Voice", icon: ""},
+                {name: "SFX", icon: ""},
+                {name: "Music", icon: ""},
+                {name: "Subtitles?", icon: ""},
+            ]}, {
+            name: "Gameplay",
+            icon: "",
+            }]
+        }, {
+        name: "Credits",
+        icon: "",
+        }, {
+        name: "Quit",
+        icon: "",
+        }
+    ]
+};
+
+ko.applyBindings(data);
+
 var menu = CreateMultiLevelMenu(document.querySelector('.mp-menu#main-menu'),
                                 document.createElement('a'));
